b7f523f00a2494e604249b5560b607f5a3e3eecf
Smooth Pudding <dummy@address>
2025-11-14 23:51:24 +0900

    Solved ID3

--- id0003/src/main.rs
+++ id0003/src/main.rs
@@ -1,3 +1,16 @@
+// <p>The prime factors of $13195$ are $5, 7, 13$ and $29$.</p>
+// <p>What is the largest prime factor of the number $600851475143$?</p>
+
+const TARGET: u64 = 600_851_475_143;
+
 fn main() {
-    println!("Hello, world!");
+    let mut num = TARGET;
+    let mut divisor = 3;
+    while divisor < num {
+        while num % divisor == 0 {
+            num /= divisor;
+        }
+        divisor += 2;
+    }
+    println!("{num}");
 }
