f9fbaf3fb06fed087cc2c023fe9033bafc04872c
Smooth Pudding <dummy@address>
2025-11-15 00:04:50 +0900

    Solved ID5

--- id0005/src/main.rs
+++ id0005/src/main.rs
@@ -2,5 +2,7 @@
 // <p>What is the smallest positive number that is <strong class="tooltip">evenly divisible<span class="tooltiptext">divisible with no remainder</span></strong> by all of the numbers from $1$ to $20$?</p>
 
 fn main() {
-    println!("Hello, world!");
+    // The product of the max power of primes that are less than 20
+    let answer: u32 = 16 * 9 * 5 * 7 * 11 * 13 * 17 * 19;
+    println!("{answer}");
 }
