b1055f60c052f2923b2ab0a889c9907225c27eed
Smooth Pudding <dummy@address>
2025-11-15 08:34:04 +0900

    Solved ID10

--- id0010/src/main.rs
+++ id0010/src/main.rs
@@ -2,5 +2,26 @@
 // <p>Find the sum of all the primes below two million.</p>
 
 fn main() {
-    println!("Hello, world!");
+    // Sieve of Eratosthenes
+    const UPPER_BOUND: usize = 2_000_000;
+    let mut is_prime = [true; UPPER_BOUND + 1];
+    is_prime[0] = false;
+    is_prime[1] = false;
+    for p in 0..=UPPER_BOUND {
+        if !is_prime[p] {
+            continue;
+        }
+        for mul in ((2 * p)..=UPPER_BOUND).step_by(p) {
+            is_prime[mul] = false;
+        }
+    }
+
+    let mut sum = 0;
+    for (p, &is_p) in is_prime.iter().enumerate() {
+        if !is_p {
+            continue;
+        }
+        sum += p;
+    }
+    println!("{sum}");
 }
