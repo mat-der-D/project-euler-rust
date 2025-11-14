772fcbbb592cb746ddfb6ddad1bb621950496252
Smooth Pudding <dummy@address>
2025-11-15 00:14:04 +0900

    Solved ID6

--- id0006/src/main.rs
+++ id0006/src/main.rs
@@ -6,5 +6,13 @@
 // <p>Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.</p>
 
 fn main() {
-    println!("Hello, world!");
+    // 1^2 + 2^2 + ... + n^2 = n*(n + 1)*(2n + 1)/6
+    // (1 + 2 + ... + n)^2 = n^2 (n + 1)^2 / 4
+    // Thus, the latter minus the former equals:
+    // n^2 (n + 1)^2 /4 - n*(n + 1)*(2n + 1)/6
+    // = { 3*n*(n + 1) - 2*(2n + 1) }*n*(n + 1)/12
+    // = (3*n^2 -n -2)*n*(n + 1)/12
+
+    let n = 100_u32;
+    println!("{}", (3 * n * n - n - 2) * n * (n + 1) / 12);
 }
