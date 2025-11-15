b97f9123e9055064a179dda0933094a16429438e
Smooth Pudding <dummy@address>
2025-11-15 18:24:01 +0900

    Solved ID32

--- id0032/src/main.rs
+++ id0032/src/main.rs
@@ -6,6 +6,68 @@
 //
 // <div class="note">HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.</div>
 
+use std::collections::HashSet;
+
+fn is_good_pair(x: u32, y: u32) -> bool {
+    let z = x * y;
+    let mut chars: Vec<char> = format!("{x}{y}{z}").chars().collect();
+    chars.sort();
+    let string: String = chars.into_iter().collect();
+    string.as_str() == "123456789"
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Find out a required condition for the problem.
+    // Let x, y, z be the triple that satisfies the condition,
+    // and let a, b, c be their number of digits respectively.
+    // This means that:
+    // - 10^(a - 1) <= x < 10^a
+    // - 10^(b - 1) <= y < 10^b
+    // - 10^(c - 1) <= z < 10^c
+    // Now, from x*y = z,
+    // - 10^(a + b - 2) <= x*y = z < 10^c
+    // - 10^(c - 1) <= z = x*y < 10^(a + b)
+    // are obtained. Hence,
+    //   c - 1 < a + b <= c + 2
+    // holds. By the way, (x, y, z) is pandigital, so
+    //   a + b + c = 9
+    // holds. From the above conditions, we obtain
+    //   3.5 <= c < 5.
+    // Then c = 4 because c is an integer. Moreover
+    //   a + b = 5
+    // satisfies. This is useful requirement condition.
+    // If we suppose a < b without loss of generality, we get that
+    //   (a, b) = (1, 4), (2, 3)
+    // are all cases we have to consider.
+
+    let mut products = HashSet::new();
+
+    for n1 in 1..=9 {
+        for n2 in (1..=9).filter(|n| ![n1].contains(n)) {
+            for n3 in (1..=9).filter(|n| ![n1, n2].contains(n)) {
+                for n4 in (1..=9).filter(|n| ![n1, n2, n3].contains(n)) {
+                    for n5 in (1..=9).filter(|n| ![n1, n2, n3, n4].contains(n)) {
+                        // ** Case 1: (a, b) = (1, 4) **
+                        let x = n1;
+                        let y = 1000 * n2 + 100 * n3 + 10 * n4 + n5;
+                        if is_good_pair(x, y) {
+                            products.insert(x * y);
+                            println!("{} x {} = {}", x, y, x * y);
+                        }
+
+                        // ** Case 2: (a, b) = (2, 3) **
+                        let x = 10 * n1 + n2;
+                        let y = 100 * n3 + 10 * n4 + n5;
+                        if is_good_pair(x, y) {
+                            products.insert(x * y);
+                            println!("{} x {} = {}", x, y, x * y);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    let total: u32 = products.into_iter().sum();
+    println!("Total = {total}");
 }
