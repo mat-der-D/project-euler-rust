dcd27e29a9953cb4bf31b162f1bb54755a42506b
Smooth Pudding <dummy@address>
2025-11-15 13:33:25 +0900

    Solved ID22

--- id0022/src/main.rs
+++ id0022/src/main.rs
@@ -2,6 +2,40 @@
 // <p>For example, when the list is sorted into alphabetical order, COLIN, which is worth $3 + 15 + 12 + 9 + 14 = 53$, is the $938$th name in the list. So, COLIN would obtain a score of $938 \times 53 = 49714$.</p>
 // <p>What is the total of all the name scores in the file?</p>
 
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufReader, Read},
+    path::Path,
+};
+
+fn char_to_value(c: char) -> u32 {
+    let raw: u32 = c.into();
+    raw - 64
+}
+
+fn evaluate_name(name: &str) -> u32 {
+    name.chars().map(|c| char_to_value(c)).sum()
+}
+
+fn load_names(path: &Path) -> Result<Vec<String>, anyhow::Error> {
+    let fs = File::open(path)?;
+    let mut reader = BufReader::new(fs);
+    let mut content = String::new();
+    reader.read_to_string(&mut content)?;
+
+    let names: Vec<String> = content.split(",").map(|s| s.replace("\"", "")).collect();
+    Ok(names)
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let path = Path::new("src/0022_names.txt");
+    let mut names = load_names(&path)?;
+    names.sort();
+    let total_score: u32 = names
+        .into_iter()
+        .enumerate()
+        .map(|(i, name)| (i as u32 + 1) * evaluate_name(&name))
+        .sum();
+    println!("{total_score}");
+    Ok(())
 }
