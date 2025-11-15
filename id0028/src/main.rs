290f4cfd29d9d03d470206fcf18e574c850377b0
Smooth Pudding <dummy@address>
2025-11-15 15:47:00 +0900

    Solved ID28

--- id0028/src/main.rs
+++ id0028/src/main.rs
@@ -7,5 +7,16 @@
 // <p>What is the sum of the numbers on the diagonals in a $1001$ by $1001$ spiral formed in the same way?</p>
 
 fn main() {
-    println!("Hello, world!");
+    // New corner numbers of n by n spiral are written as follows:
+    // - upper right: n^2
+    // - upper left: n^2 - n + 1
+    // - lower left: n^2 - 2n + 2
+    // - lower right: n^2 - 3n + 3
+    // The sum of them is 4n^2 -6n +6
+
+    let mut sum = 1;
+    for n in (3..=1001).step_by(2) {
+        sum += 4 * n * n - 6 * n + 6;
+    }
+    println!("{sum}");
 }
