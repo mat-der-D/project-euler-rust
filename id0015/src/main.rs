7eb8599286ccba71ebefcf1b1c60ee62edda502c
Smooth Pudding <dummy@address>
2025-11-15 10:31:30 +0900

    Solved ID15

--- id0015/src/main.rs
+++ id0015/src/main.rs
@@ -3,6 +3,18 @@
 // <img src="resources/images/0015.png?1678992052" class="dark_img" alt=""></div>
 // <p>How many such routes are there through a $20 \times 20$ grid?</p>
 
+fn comb(n: u128, r: u128) -> u128 {
+    if r == 0 {
+        return 1;
+    }
+    if n < r {
+        return 0;
+    }
+    comb(n - 1, r - 1) * n / r
+}
+
 fn main() {
-    println!("Hello, world!");
+    // The answer is the number of the permutations of 20 downs and 20 rights.
+    // This equals to 40C20.
+    println!("{}", comb(40, 20));
 }
